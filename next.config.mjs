ea74fd65f3aa9dd2c4afd27d894437ec48bec7f0
Echoes Admin <kota12202003@icloud.com>
2026-05-03 15:46:50 +0900

    0503 4

--- next.config.mjs
+++ next.config.mjs
@@ -15,9 +15,6 @@ const withPWA = withPWAInit({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
